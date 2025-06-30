f55e77b31748fe9823672a599249429c1bc6cdcf
Mostafa62 <test@test.com>
2025-06-30 18:13:27 +0300

    Identifying the error

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className="overflow-hidden text-neutral-300 antialiased selection:bg-blue-200 selection:text-cyan-900">
       <Background/>
-      <div className="container mx-auto px-4 md:px-14">
+      <div className="container mx-auto px-10 md:px-14">
         <Navbar />
         <Hero />
         <About />
